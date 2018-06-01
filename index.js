4f33bafa4663f7b030eb13cec735e4618dc3543e
ssigg <stephan.sigg@gmail.com>
2018-06-01 16:33:51 +0200

    Do not build archive entry files, explain everything

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ const metalsmith = require('metalsmith');
 const assets = require('metalsmith-assets');
 const drafts = require('metalsmith-drafts');
 const collections = require('metalsmith-collections');
+const ignore = require('metalsmith-ignore');
 const markdown = require('metalsmith-markdown');
 const discoverPartials = require('metalsmith-discover-partials');
 const layouts = require('metalsmith-layouts');
 
 metalsmith(__dirname)
   .metadata({
-      title: "blah blah"
+      title: "AKO"
   })
+  
+  // Handle all files in /src/content
   .source('./src/content')
+
+  // Build into /build
   .destination('./build')
+
+  // Clean build directory
   .clean(true)
+
+  // Copy assets into /build
   .use(assets({
       source: './src/assets',
       destination: '.'
   }))
+
+  // Ignore drafts
   .use(drafts())
+
+  // Compile archive collection into one single page
   .use(collections({
       archive: {
           pattern: 'archive/*.md',
@@ -25,16 +38,29 @@ metalsmith(__dirname)
           reverse: true
       }
   }))
+
+  // Apply markdown everywhere
   .use(markdown())
+
+  // Do not generate archive entries, they are compiled into one single page
+  .use(ignore('archive/*.html'))
+
+  // Find handlebars partials
   .use(discoverPartials({
     directory: './src/layouts/partials',
     pattern: /\.hbs$/
   }))
+
+  // Apply layouts
   .use(layouts({
-      directory: './src/layouts',
-      engine: 'handlebars',
-      pattern: '*.html'
+    directory: './src/layouts',
+    engine: 'handlebars',
+    pattern: '*.html'
   }))
+
+  // Run everything.
   .build(function(err, files) {
       if (err) { throw err; }
   });
+
+  // Done.
